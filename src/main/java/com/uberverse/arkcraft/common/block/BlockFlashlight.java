e83f34d5b214026083c072a35bee6e2d178a78cb
Lewis McReu <wimcreuwels@MacBook-Pro-van-Wim.local>
2016-07-10 17:30:35 +0200

    More performant Flashlight

--- src/main/java/com/uberverse/arkcraft/common/block/BlockFlashlight.java
+++ src/main/java/com/uberverse/arkcraft/common/block/BlockFlashlight.java
@@ -1,11 +1,11 @@
 package com.uberverse.arkcraft.common.block;
 
+import com.uberverse.arkcraft.common.tileentity.TileFlashlight;
+
 import net.minecraft.block.BlockAir;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
-
-import com.uberverse.arkcraft.common.tileentity.TileFlashlight;
 
 public class BlockFlashlight extends BlockAir implements ITileEntityProvider
 {
@@ -13,7 +13,6 @@ public class BlockFlashlight extends BlockAir implements ITileEntityProvider
 	{
 		super();
 		this.setLightLevel(0.8F);
-		// this.setUnlocalizedName("light_source");
 		this.setBlockBounds(0, 0, 0, 0, 0, 0);
 	}
 
--- src/main/java/com/uberverse/arkcraft/common/tileentity/TileFlashlight.java
+++ src/main/java/com/uberverse/arkcraft/common/tileentity/TileFlashlight.java
@@ -1,13 +1,15 @@
 package com.uberverse.arkcraft.common.tileentity;
 
-import net.minecraft.init.Blocks;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.network.NetworkManager;
-import net.minecraft.network.Packet;
-import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.server.gui.IUpdatePlayerListBox;
 import net.minecraft.tileentity.TileEntity;
 
+/**
+ * 
+ * @author Lewis_McReu
+ * 
+ * Credit to Jabelar for a slightly more performant implementation
+ * 
+ */
 public class TileFlashlight extends TileEntity implements IUpdatePlayerListBox
 {
 	public int ticks;
@@ -19,46 +21,11 @@ public class TileFlashlight extends TileEntity implements IUpdatePlayerListBox
 	}
 
 	@Override
-	public void readFromNBT(NBTTagCompound nbt)
-	{
-		super.readFromNBT(nbt);
-
-		ticks = nbt.getInteger("ticks");
-	}
-
-	@Override
-	public void writeToNBT(NBTTagCompound nbt)
-	{
-		super.writeToNBT(nbt);
-
-		nbt.setInteger("ticks", ticks);
-	}
-
-	@Override
-	public Packet getDescriptionPacket()
-	{
-		NBTTagCompound nbt = new NBTTagCompound();
-		writeToNBT(nbt);
-		return new S35PacketUpdateTileEntity(this.pos, getBlockMetadata(), nbt);
-	}
-
-	@Override
-	public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity pkt)
-	{
-		NBTTagCompound nbt = pkt.getNbtCompound();
-		if (nbt != null)
-		{
-			this.readFromNBT(nbt);
-		}
-	}
-
-	@Override
 	public void update()
 	{
 		if (++ticks > 2)
 		{
-			this.invalidate();
-			this.worldObj.setBlockState(pos, Blocks.air.getDefaultState());
+			this.getWorld().setBlockToAir(getPos());
 			return;
 		}
 	}
