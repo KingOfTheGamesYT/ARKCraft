d496c14534c3071a1c17dc0d3f5ac3f45a590223
SuperScary <chpwnd@gmail.com>
2016-08-11 17:15:01 -0700

    Syncing

--- src/main/java/com/uberverse/arkcraft/client/gui/GUIEngram.java
+++ src/main/java/com/uberverse/arkcraft/client/gui/GUIEngram.java
@@ -14,7 +14,6 @@ import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.util.ResourceLocation;
 
 /**
