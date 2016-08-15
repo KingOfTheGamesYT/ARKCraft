0458e40bf4d1725ea0db0c759f5bdf60474760f7
Wim Creuwels <wim.creuwels@hotmail.com>
2016-08-15 11:03:08 +0200

    Revert "pull"
    This reverts commit 7a09f0747530b0d3e064f0201b5dc48ece084c88.
    
    # Conflicts:
    
    #	src/main/java/com/uberverse/arkcraft/common/event/CommonEventHandler.java

--- src/main/java/com/uberverse/arkcraft/common/event/CommonEventHandler.java
+++ src/main/java/com/uberverse/arkcraft/common/event/CommonEventHandler.java
@@ -37,119 +37,160 @@ import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.WorldTickEvent;
 
-public class CommonEventHandler {
+public class CommonEventHandler
+{
 	public boolean destroy;
 	public boolean destroyBlocks;
 	public boolean startSwing;
 
-	public static void init() {
+	public static void init()
+	{
 		CommonEventHandler handler = new CommonEventHandler();
 		FMLCommonHandler.instance().bus().register(handler);
 		MinecraftForge.EVENT_BUS.register(handler);
 	}
 
 	@SubscribeEvent
-	public void onEntityConstructing(EntityEvent.EntityConstructing event) {
-		if (event.entity instanceof EntityPlayer && ARKPlayer.get((EntityPlayer) event.entity) == null) {
+	public void onEntityConstructing(EntityEvent.EntityConstructing event)
+	{
+		if (event.entity instanceof EntityPlayer && ARKPlayer
+				.get((EntityPlayer) event.entity) == null)
+		{
 			ARKPlayer.register((EntityPlayer) event.entity, event.entity.worldObj);
 			if (event.entity.worldObj.isRemote) // On client
 			{
 				LogHelper.info("ARKPlayerEventHandler: Registered a new ARKPlayer on client.");
-			} else {
+			}
+			else
+			{
 				LogHelper.info("ARKPlayerEventHandler: Registered a new ARKPlayer on server.");
 			}
 		}
 	}
 
 	@SubscribeEvent
-	public void onClonePlayer(PlayerEvent.Clone event) {
+	public void onClonePlayer(PlayerEvent.Clone event)
+	{
 		LogHelper.info("ARKPlayerEventHandler: Cloning player extended properties");
 		ARKPlayer.get(event.entityPlayer).copy(ARKPlayer.get(event.original));
 	}
 
 	@SubscribeEvent
 	@SuppressWarnings({ "unchecked" })
-	public void onLivingUpdateEvent(LivingEvent.LivingUpdateEvent event) {
+	public void onLivingUpdateEvent(LivingEvent.LivingUpdateEvent event)
+	{
 		// LogHelper.info("LIVING UPDATE EVENT");
-		if (event.entity instanceof EntityPlayer) {
+		if (event.entity instanceof EntityPlayer)
+		{
 			EntityPlayer player = (EntityPlayer) event.entity;
 			// Enable pooping once every (the value in the config) ticks
-			if (player.ticksExisted % ModuleItemBalance.PLAYER.TICKS_BETWEEN_PLAYER_POOP == 0) {
+			if (player.ticksExisted % ModuleItemBalance.PLAYER.TICKS_BETWEEN_PLAYER_POOP == 0)
+			{
 				ARKPlayer.get(player).setCanPoop(true);
 			}
+		}
+	}
 
-			// TODO Find a cleaner way to do this.
-			World worldIn = player.getEntityWorld();
+	@SubscribeEvent
+	public void onWorldTick(WorldTickEvent event)
+	{
+		if (event.side.isServer())
+		{
+			World world = event.world;
 			// The item will spawn on the server side. If you don't check, it
 			// will run on client and create a 'phantom' item
-			if (worldIn != null && !worldIn.isRemote) {
-				List<Entity> entities = worldIn.loadedEntityList;
-				for (int i = 0; i < entities.size(); i++) {
-					for (int j = 1; j < entities.size(); j++) {
-						if (entities.get(i) instanceof EntityItem && entities.get(j) instanceof EntityItem) {
-							EntityItem target = (EntityItem) entities.get(i);
-							EntityItem targetJ = (EntityItem) entities.get(j);
+			if (!world.isRemote)
+			{
+				if (bookSpawnDelay > 0) bookSpawnDelay--;
+				else
+				{
+					List<Entity> entities = world.loadedEntityList;
+					for (int i = 0; i < entities.size(); i++)
+					{
+						for (int j = 1; j < entities.size(); j++)
+						{
+							if (entities.get(i) instanceof EntityItem && entities
+									.get(j) instanceof EntityItem)
+							{
+								EntityItem target = (EntityItem) entities.get(i);
+								EntityItem targetJ = (EntityItem) entities.get(j);
 
-							int x = target.getPosition().getX();
-							int y = target.getPosition().getY();
-							int z = target.getPosition().getZ();
+								int x = target.getPosition().getX();
+								int y = target.getPosition().getY();
+								int z = target.getPosition().getZ();
 
-							int xJ = targetJ.getPosition().getX();
-							int yJ = targetJ.getPosition().getY();
-							int zJ = targetJ.getPosition().getZ();
+								int xJ = targetJ.getPosition().getX();
+								int yJ = targetJ.getPosition().getY();
+								int zJ = targetJ.getPosition().getZ();
 
-							Item item = target.getEntityItem().getItem();
-							Item itemJ = targetJ.getEntityItem().getItem();
-							// the two items must be at least 3 blocks close
-							// to
-							// each other in xy directions, must be 1 block
-							// close in z.
+								Item item = target.getEntityItem().getItem();
+								Item itemJ = targetJ.getEntityItem().getItem();
 
-							if (Math.abs(x - xJ) <= 3 && Math.abs(y - yJ) <= 3 && Math.abs(z - zJ) <= 1) {
-								// Check if items are in water
-								//Block dest = worldIn.getBlockState(target.getPosition()).getBlock();
-								//Block destJ = worldIn.getBlockState(targetJ.getPosition()).getBlock();
-								//if (dest == Blocks.water && destJ == Blocks.water) {
-									if (item == Items.book && itemJ == Items.bone) {
-										if (target.ticksExisted > 100 && targetJ.ticksExisted > 100) {
-											target.getEntityItem().stackSize -= 1;
-											targetJ.getEntityItem().stackSize -= 1;
-											if(target.getEntityItem().stackSize == 0) target.setDead();
-											if(targetJ.getEntityItem().stackSize == 0) targetJ.setDead();
-											WorldServer worldServer = (WorldServer)worldIn;
-											worldServer.spawnParticle(EnumParticleTypes.SMOKE_LARGE, false, x + 0.5D, y+ 1.0D, z+ 0.5D, 1, 0.0D, 0.0D, 0.0D, 0.0D, new int[0]);
-											if (target.isDead && targetJ.isDead) {
-												worldIn.spawnEntityInWorld(new EntityItem(worldIn, x, y, z,
-														new ItemStack(ARKCraftItems.info_book)));
-   
-												
-											}
+								if ((item == Items.book && itemJ == Items.bone) || (item == Items.bone && itemJ == Items.book) && !target.isDead && !targetJ.isDead)
+								{
+									if (target.ticksExisted > 100 && targetJ.ticksExisted > 100)
+									{
+										// the two items must be at least 3
+										// blocks close
+										// to
+										// each other in xy directions, must be
+										// 1 block
+										// close in z.
+										if (Math.abs(x - xJ) <= 3 && Math.abs(y - yJ) <= 1 && Math
+												.abs(z - zJ) <= 3)
+										// Check if items are in water
+										// Block dest =
+										// worldIn.getBlockState(target.getPosition()).getBlock();
+										// Block destJ =
+										// worldIn.getBlockState(targetJ.getPosition()).getBlock();
+										// if (dest == Blocks.water && destJ ==
+										// Blocks.water) {
+										{
+											bookSpawnDelay += 20;
+											target.getEntityItem().stackSize--;
+											targetJ.getEntityItem().stackSize--;
+											if (target.getEntityItem().stackSize == 0) world
+													.removeEntity(target);
+											if (targetJ.getEntityItem().stackSize == 0) world
+													.removeEntity(targetJ);
+											WorldServer worldServer = (WorldServer) world;
+											worldServer.spawnParticle(EnumParticleTypes.SMOKE_LARGE,
+													false, x + 0.5D, y + 1.0D, z + 0.5D, 1, 0.0D,
+													0.0D, 0.0D, 0.0D, new int[0]);
+											world.spawnEntityInWorld(new EntityItem(world, x, y, z,
+													new ItemStack(ARKCraftItems.info_book)));
 										}
 									}
-								//}
+								}
 							}
 						}
-
 					}
 				}
 			}
 		}
 	}
 
+	public static int bookSpawnDelay = 0;
+
 	public static int count;
 
 	@SuppressWarnings("static-access")
 	@SubscribeEvent
-	public void playerInteract(PlayerInteractEvent event) {
+	public void playerInteract(PlayerInteractEvent event)
+	{
 
 		Action eventAction = event.action;
 		ItemStack item = event.entityPlayer.getCurrentEquippedItem();
 
-		if (item != null && item.getItem() instanceof ItemRangedWeapon) {
-			if (eventAction.RIGHT_CLICK_BLOCK != null & eventAction.RIGHT_CLICK_AIR != null) {
+		if (item != null && item.getItem() instanceof ItemRangedWeapon)
+		{
+			if (eventAction.RIGHT_CLICK_BLOCK != null & eventAction.RIGHT_CLICK_AIR != null)
+			{
 				ObfuscationReflectionHelper.setPrivateValue(ItemRenderer.class,
-						Minecraft.getMinecraft().getItemRenderer(), 1F, "equippedProgress", "field_78454_c");
+						Minecraft.getMinecraft().getItemRenderer(), 1F, "equippedProgress",
+						"field_78454_c");
 			}
 		}
 
@@ -159,17 +200,24 @@ public class CommonEventHandler {
 	// for (int z = -checkSize; z <= checkSize; z++) {
 	// for (int y = 0; y <= checkSize; y++) {
 
-	public void destroyBlocks(World world, BlockPos pos, boolean first) {
+	public void destroyBlocks(World world, BlockPos pos, boolean first)
+	{
 		// int checkSize = 1;
 		int i = pos.getX();
 		int j = pos.getY();
 		int k = pos.getZ();
-		for (int x = i - 1; x <= i + 1; x++) {
-			for (int z = k - 1; z <= k + 1; z++) {
-				for (int y = j - 1; y <= j + 1; y++) {
-					if (first || (x != i && y != j && k != z)) {
+		for (int x = i - 1; x <= i + 1; x++)
+		{
+			for (int z = k - 1; z <= k + 1; z++)
+			{
+				for (int y = j - 1; y <= j + 1; y++)
+				{
+					if (first || (x != i && y != j && k != z))
+					{
 						IBlockState blockState = world.getBlockState(new BlockPos(x, y, z));
-						if (blockState.getBlock() == Blocks.log || blockState.getBlock() == Blocks.log2) {
+						if (blockState.getBlock() == Blocks.log || blockState
+								.getBlock() == Blocks.log2)
+						{
 							// world.destroyBlock(new BlockPos(x, y, z), true);
 							count++;
 							this.destroyBlocks(world, new BlockPos(x, y, z), false);
@@ -184,19 +232,23 @@ public class CommonEventHandler {
 	// (ClientEventhanlder.arkMode()) is the issues
 	public static boolean arkMode;
 
-	public boolean arkMode() {
+	public boolean arkMode()
+	{
 		return arkMode;
 	}
 
 	@SubscribeEvent
-	public void breakSpeed(BreakSpeed event) {
+	public void breakSpeed(BreakSpeed event)
+	{
 		System.out.println(arkMode);
-		if (arkMode && event.entityPlayer.getHeldItem() != null
-				&& event.entityPlayer.getHeldItem().getItem() instanceof ARKCraftTool) {
+		if (arkMode && event.entityPlayer.getHeldItem() != null && event.entityPlayer.getHeldItem()
+				.getItem() instanceof ARKCraftTool)
+		{
 			destroyBlocks(event.entityPlayer.worldObj, event.pos, true);
 			float f = count / 20F;
 			System.out.println(count + " " + f);
-			if (count > 0) {
+			if (count > 0)
+			{
 				event.newSpeed = event.originalSpeed / f / 10;
 			}
 			count = 0;
@@ -208,16 +260,21 @@ public class CommonEventHandler {
 	public static int ticksSwing = 0;
 
 	@SubscribeEvent
-	public void onPlayerTick(PlayerTickEvent evt) {
+	public void onPlayerTick(PlayerTickEvent evt)
+	{
 
 		EntityPlayer p = evt.player;
 		ItemStack stack = p.getCurrentEquippedItem();
 
-		if (stack != null && stack.getItem() instanceof ItemRangedWeapon) {
+		if (stack != null && stack.getItem() instanceof ItemRangedWeapon)
+		{
 			ItemRangedWeapon w = (ItemRangedWeapon) stack.getItem();
-			if (w.isReloading(stack)) {
-				if (++reloadTicks >= w.getReloadDuration()) {
-					if (!p.worldObj.isRemote) {
+			if (w.isReloading(stack))
+			{
+				if (++reloadTicks >= w.getReloadDuration())
+				{
+					if (!p.worldObj.isRemote)
+					{
 						w.setReloading(stack, p, false);
 						reloadTicks = 0;
 						w.hasAmmoAndConsume(stack, p);
