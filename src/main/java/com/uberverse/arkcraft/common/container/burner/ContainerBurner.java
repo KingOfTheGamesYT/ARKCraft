ae1a39f40a5d399ea459f621536f4dcf5f382481
Wim Creuwels <wim.creuwels@hotmail.com>
2016-09-06 21:16:26 +0200

    More burner fixes

--- src/main/java/com/uberverse/arkcraft/common/container/burner/ContainerBurner.java
+++ src/main/java/com/uberverse/arkcraft/common/container/burner/ContainerBurner.java
@@ -39,6 +39,7 @@ public abstract class ContainerBurner extends Container implements IBurnerContai
 		}
 
 		slotEnd = slotCounter;
+		playerSlotStart = slotCounter+1;
 		// Player hotbar
 		for (int x = 0; x < 9; x++)
 		{
