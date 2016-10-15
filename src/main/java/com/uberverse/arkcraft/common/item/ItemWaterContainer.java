18f202857cb3ca09ca26fb7d6cbdd260d0bd3dea
Ronald Goedeke <ronald.goedeke@outlook.com>
2016-10-15 21:25:57 +0200

    Gui Texture

---
+++ src/main/java/com/uberverse/arkcraft/common/item/ItemWaterContainer.java
@@ -0,0 +1,22 @@
+package com.uberverse.arkcraft.common.item;
+
+public class ItemWaterContainer extends ARKCraftItem
+{
+	int water;
+	
+	public ItemWaterContainer(int water)
+	{
+		this.water = water;
+	}
+	
+	public int getwater()
+	{
+		return water;
+	}
+	
+	public int setWater(int water)
+	{
+		return water;
+	}
+	
+}
