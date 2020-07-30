b51980890cdd860f3a6c0d0ea828ae2b2752f6a6
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2020-07-30 13:55:47 -0400

    Fixed Weapons based atribute
    Made it work

--- src/main/java/com/uberverse/arkcraft/common/data/WeaponModAttributes.java
+++ src/main/java/com/uberverse/arkcraft/common/data/WeaponModAttributes.java
@@ -1,15 +1,16 @@
 package com.uberverse.arkcraft.common.data;
 
-import net.minecraft.entity.SharedMonsterAttributes;
+
 import net.minecraft.entity.ai.attributes.BaseAttribute;
 import net.minecraft.entity.ai.attributes.RangedAttribute;
 
-public class WeaponModAttributes extends SharedMonsterAttributes
+public class WeaponModAttributes
 {
 	// TODO check necessity
-	public static final BaseAttribute RELOAD_TIME = (new RangedAttribute((BaseAttribute) null, "arkcraft.reloadTime",
+	public static  BaseAttribute RELOAD_TIME = (new RangedAttribute((BaseAttribute) null, "arkcraft.reloadTime",
 			0D, 0D, Double.MAX_VALUE));
-	public static final BaseAttribute CONSUME_ITEM = (new RangedAttribute((BaseAttribute) null, "arkcraft.consumeItem",
+	public static  BaseAttribute CONSUME_ITEM = (new RangedAttribute((BaseAttribute) null, "arkcraft.consumeItem",
 			0D, 0D, Double.MAX_VALUE));
+
 
 }
