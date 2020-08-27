053cf458d602bcc8e92005eb32bf3bfb1cffe468
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2020-08-26 23:48:06 -0400

    Added back the Dodo
    Added back the dodo

--- src/main/java/com/uberverse/arkcraft/init/ARKCraftEntities.java
+++ src/main/java/com/uberverse/arkcraft/init/ARKCraftEntities.java
@@ -1,9 +1,13 @@
 package com.uberverse.arkcraft.init;
 
+import com.uberverse.arkcraft.ARKCraft;
+import com.uberverse.arkcraft.common.entity.EntityDodo;
+import com.uberverse.arkcraft.common.handlers.EntityHandler;
+
 public class ARKCraftEntities
 {
 	public static void init()
 	{
-		//EntityHandler.registerMonster(EntityDodo.class, ARKCraft.MODID + ".dodo");
+		EntityHandler.registerMonster(EntityDodo.class, ARKCraft.MODID + ".dodo");
 	}
 }
